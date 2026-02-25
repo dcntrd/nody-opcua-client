f0c739b1901eabaa654429e6fba8629795abb97a
fm <dev@decentered.net>
2026-02-25 22:02:12 +0100

    addded log4j 2.12.4

--- src/main/java/net/decentered/nody/opcua/client/Main.java
+++ src/main/java/net/decentered/nody/opcua/client/Main.java
@@ -1,17 +1,15 @@
 package net.decentered.nody.opcua.client;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    static void main() {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        IO.println(String.format("Hello and welcome!"));
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            IO.println("i = " + i);
-        }
+public class Main {
+
+    private static final Logger logger = LogManager.getLogger(Main.class);
+
+    static void main() {
+
+        logger.info("Starting Client");
+
     }
 }
