6e3fa229f42c438e9c8f7ccd22b5e353a8c682b4
dcntrd <63446241+dcntrd@users.noreply.github.com>
2026-03-18 12:43:03 +0100

    Change main method to public static

--- src/main/java/net/decentered/nody/opcua/client/Main.java
+++ src/main/java/net/decentered/nody/opcua/client/Main.java
@@ -11,7 +11,7 @@ public class Main {
 
     private static final Logger logger = LogManager.getLogger(Main.class);
 
-    static void main(String[] args) {
+    public static void main(String[] args) {
 
         logger.info("Starting Client");
 
